ce4c7d78420358651d22bef1a8738dc0b363626f
Ronel028 <floridaronel15@gmail.com>
2024-09-09 19:07:51 +0800

    initial commit

---
+++ resources/js/Pages/Home.jsx
@@ -0,0 +1,9 @@
+const Home = () => {
+    return (
+        <>
+            <h1>Hello! This is boilerplate for Laravel, React and Inertia without laravel breeze and jetsream.</h1>
+        </>
+    )
+}
+
+export default Home;
